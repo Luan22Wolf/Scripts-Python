13dbdbdecf20601233d6286a98f170e866d7f0e1
Luan_Lobo <149533975+Luan22Wolf@users.noreply.github.com>
2025-04-22 20:46:50 -0300

    Atualizar o geradorQRcode.py

--- Gerador de QRcode/geradorQRcode.py
+++ Gerador de QRcode/geradorQRcode.py
@@ -1,14 +1,15 @@
 import pyqrcode
-import png 
+import png
 import pprint
 
-#Input para colar seu QRCode
 qrcode = input(str("Cole o link que deseja criar um QRCode: "))
 
-#qr_code_tring = "https://inscricao.prozeducacao.com.br/home-2024/?utm_source=google&utm_medium=cpc&utm_campaign=MG_Google_Search_Institucional_CPC&gad_source=1&gclid=EAIaIQobChMI7MvY1tHnhQMVClpIAB1jaQexEAAYASAAEgIx4_D_BwE#"
-
-# Monta o QRCode
+# Aqui é criado o qr code a partir do link informado no input
 url = pyqrcode.create(qrcode)
 
-# Salva o QRCode
+# Salva o qr code como imagem PNG com a escala 8 (pra definir o tamanho dos blocos)
+# O arquivo será salvo como 'qr.png' na mesma pasta do script
 url.png(r'qr.png', scale=8)
+
+#Ou pode escolher algum caminho para salvar o qr code, exemplo ==>> 
+#url.png("/storage/emulated/0/Download/qr.png", scale=8)
