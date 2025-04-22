8863806fa4210020806d33ca308dfc90ccdc53e6
Luan_Lobo <149533975+Luan22Wolf@users.noreply.github.com>
2025-04-22 20:32:52 -0300

    Atualizar o qr.py

--- Gerador de QRcode/qr.py
+++ Gerador de QRcode/qr.py
@@ -1,4 +1,7 @@
 import qrcode
 
 meu_qrcode = qrcode.make("https://enferminas.prozeducacao.com.br/")
-meu_qrcode.save("")
+meu_qrcode.save("meu_qrcode.png")
+
+#se quiser salvar em algum caminho especifico, exemplo usado aqui por min no telefone é ==>>
+#meu_qrcode.save("/storage/emulated/0/Download/meu_qrcode.png")
